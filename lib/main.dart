7a1614547064f0f73b6f2491ad3971ce5749b183
vasim1234 <vasimaiyub@gmail.com>
2026-08-27 22:56:20 +0530

    Change home widget to PlayerScreen and update title
    Updated the main application to use PlayerScreen as the home widget and changed the app title.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/player_screen.dart';
 
 void main() {
   runApp(const MyMusicApp());
@@ -10,49 +11,13 @@ class MyMusicApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Music',
+      title: 'My Music 3D',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
-        primarySwatch: Colors.deepPurple,
         scaffoldBackgroundColor: const Color(0xFF121212),
       ),
-      home: const MainDashboard(),
+      home: const PlayerScreen(),
     );
   }
 }
-
-class MainDashboard extends StatelessWidget {
-  const MainDashboard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Music 3D'),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(Icons.music_note_rounded, size: 80, color: Colors.deepPurpleAccent),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to Fresh 3D Music Player',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Building clean & error-free structure...',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
