68e1e964539968463e2358954166653a61035a7e
vasim1234 <vasimaiyub@gmail.com>
2026-09-01 19:11:32 +0530

    Add position, duration, and player state streams
    Added streams for position, duration, and player state in MyAudioHandler class.

--- lib/services/background_handler.dart
+++ lib/services/background_handler.dart
@@ -103,7 +103,11 @@ class MyAudioHandler extends BaseAudioHandler {
   Future<void> seek(Duration position) async {
     await _player.seek(position);
   }
+// Add these in MyAudioHandler class:
 
+Stream<Duration> get positionStream => _player.positionStream;
+Stream<Duration?> get durationStream => _player.durationStream;
+Stream<PlayerState> get playerStateStream => _player.playerStateStream;
   PlaybackState _transformEvent(PlaybackEvent event) {
     return PlaybackState(
       controls: [
